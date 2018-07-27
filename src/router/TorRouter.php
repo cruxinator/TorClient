6eba7a017868c61403dc998346a1dcf952ae422f
c-harris <c.harris@communicationinnovations.com.au>
2018-07-28 00:17:43 +1000

    cleaned up TorRouter to work with async sockets

--- src/router/TorRouter.php
+++ src/router/TorRouter.php
@@ -2,6 +2,8 @@
 namespace cruxinator\TorClient\router;
 use cruxinator\TorClient\Lib\BigInteger;
 use cruxinator\TorClient\Lib\Logger;
+use \Workerman\Worker;
+use \Workerman\Connection\AsyncTcpConnection;
 
 class TorRouter
 {
@@ -10,19 +12,7 @@ class TorRouter
      */
     private static $routerlog;
     /**
-     * @var DataInputStream
-     */
-    private $din;
-    /**
-     * @var DataOutputStream
-     */
-    private $dout;
-    /**
-     * @var BufferedReader
-     */
-    private $br;
-    /**
-     * @var Socket
+     * @var \Workerman\Connection\ConnectionInterface
      */
     private $router;
     /**
@@ -38,17 +28,9 @@ class TorRouter
      */
     private $D;
     /**
-     * @var byte[]
-     */
-    private $data;
-    /**
-     * @var byte[]
-     */
-    private $decryptedData;
-    /**
      * @var String
      */
-    private $DirIP = "192.168.0.111";
+    private $DirIP = "127.0.0.1";
     //BigInt
     /**
      * @var BigInteger
@@ -98,15 +80,11 @@ class TorRouter
         $OR->loginit();
         $OR->genKey();
         $OR->sendToDir();
-        while (true) {
-            $OR->data = $OR->getData();
-            print(count($OR->data));
-            $OR->decryptedData = $OR->decrypt(count($OR->data));
-            print(self::bytesToString($OR->decryptedData));
-            $OR->sendData();
-
+        $OR->getData();
+        if(!defined('GLOBAL_START'))
+        {
+            Worker::runAll();
         }
-
     }
 
     private function loginit()
@@ -142,7 +120,7 @@ class TorRouter
         $this->phi = $this->p->sub(BigInteger::ONE())->mul($this->q->sub(BigInteger::ONE()));
         $this->e   = BigInteger::probablePrime($bitlength / 2);
 
-        while ($this->phi->gcd(e)->compareTo(BigInteger::ONE()) > 0 && $this->e->compareTo($this->phi) < 0) {
+        while ($this->phi->gcd($this->e)->compareTo(BigInteger::ONE()) > 0 && $this->e->compareTo($this->phi) < 0) {
             $this->e->add(BigInteger::ONE());
         }
 
@@ -155,17 +133,21 @@ class TorRouter
     private function sendToDir()
     {
         try {
-            $this->router = new Socket($this->DirIP, $this->DirPort);
-            $this->dout   = new DataOutputStream($this->router->getOutputStream());
-            $this->dout->writeUTF("0/" . $this->E[0] . "/" . $this->N[0] . "/" .
-                $this->E[1] . "/" . $this->N[1] . "/" . $this->E[2] . "/" . $this->N[2]);
+            $this->router = new AsyncTcpConnection('tcp://'.$this->DirIP.':'.$this->DirPort);
+            /**
+             * @param \Workerman\Connection\ConnectionInterface $remote_connection
+             */
+            $this->router->onConnect = function($remote_connection)
+            {
+                $remote_connection->send("0/" . $this->E[0] . "/" . $this->N[0] . "/" .
+                    $this->E[1] . "/" . $this->N[1] . "/" . $this->E[2] . "/" . $this->N[2]);
+            };
+            $this->router->connect();
 
             for ($i = 0; $i < 3; $i++) {
                 print("E[" . $i . "]=" . $this->E[$i]);
                 print("N[" . $i . "]=" . $this->N[$i]);
             }
-            $this->dout->flush();
-            $this->dout->close();
             $this->router->close();
         }
         catch (\Exception $ex) {
@@ -175,37 +157,48 @@ class TorRouter
     }
 
     /**
-     * @return array|null|byte[]
+     * @return array|null|
      */
     private function getData()
     {
         self::$routerlog->info("Waiting to receive data.");
         try {
-            $RouterAsServer = new ServerSocket($this->RouterPort, 10);
-            $DataSender     = $RouterAsServer->accept();
-            self::$routerlog->info("Connection with client established.");
-            $this->din    = new DataInputStream($DataSender->getInputStream());
-            $len          = $this->din->readInt();
-            $receivedData = array(); // byte array of $len
-            $this->din->readFully($receivedData);
-            $this->din->close();
-            $DataSender->close();
-            $RouterAsServer->close();
-            self::$routerlog->info("Data Received.");
-            print("Data received in Bytes" . $this->bytesToString($receivedData));
-            return $receivedData;
+            //$RouterAsServer = new ServerSocket($this->RouterPort, 10);
+            $RouterAsServer= new Worker('tcp://0.0.0.0:' . $this->RouterPort);
+            /**
+             * @param \Workerman\Connection\ConnectionInterface  $incoming
+             */
+            $RouterAsServer->onConnect= function($incoming)
+            {
+                self::$routerlog->info("Connection with client established. " . $incoming->getRemoteIp());
+            };
+            $outter = $this;
+            /**
+             * @param \Workerman\Connection\ConnectionInterface $connection
+             * @param [] $buffer
+             */
+            $RouterAsServer->onMessage = function($connection, $buffer) {
+                $receivedData = $buffer;
+                self::$routerlog->info("Data Received.");
+                print("Data received in Bytes" . $this->bytesToString($receivedData));
+                print(count($receivedData));
+                $receivedDataDecrpt  = $this->decrypt($receivedData, count($receivedData));
+                print(self::bytesToString($receivedDataDecrpt));
+                $this->sendData($receivedDataDecrpt);
+                $connection->close();
+            };
+
         }
         catch (\Exception $ex) {
             self::$routerlog->severe("Data receiving failed.");
         }
-        return null;
     }
 
     /**
      * @param int $len
-     * @return array|byte[]
+     * @return array
      */
-    private function decrypt($len)
+    private function decrypt($data, $len)
     {
         $key = 0;
         if ($len == 256 || $len == 257) {
@@ -215,30 +208,33 @@ class TorRouter
         }
         print("len=" . $len);
         print("keys to be used " . $key);
-        print("Data to decrypt in bytes: " . self::bytesToString($this->data));
-        return (new BigInteger($this->data))->modPow(new BigInteger($this->D[$key]),new BigInteger($this->N[$key]))->toByteArray();
+        print("Data to decrypt in bytes: " . self::bytesToString($data));
+        return (new BigInteger($data))->modPow(new BigInteger($this->D[$key]),new BigInteger($this->N[$key]))->toByteArray();
 
     }
 
-    private function sendData()
+    private function sendData($decryptedData)
     {
-        print("decypted data in string: " . self::bytesToString($this->decryptedData));
-        $st     = new StringTokenizer(self::bytesToString($this->decryptedData), "::");
-        $nextIp = $st->nextToken();
-        $m      = $st->nextToken();
+        print("decypted data in string: " . self::bytesToString($decryptedData));
+        $st     = explode("::",self::bytesToString($decryptedData));
+        $nextIp = $st[0];
+        $m      = $st[0];
         $msg    = unpack('C*', trim($m));
         print("next peel: " . self::bytesToString($msg));
         $l = count($msg);
         try {
+            $RouterAsClient = new AsyncTcpConnection('tcp://'.$nextIp.':'.$this->RouterPort);
+            /**
+             * @param \Workerman\Connection\ConnectionInterface $remote_connection
+             */
+            $RouterAsClient->onConnect = function($remote_connection) use($l,$msg,$m)
+            {
+                $remote_connection->send($l);
+                $remote_connection->send($m);
+                $remote_connection->close();
+            };
+            $RouterAsClient->connect();
 
-            print("so you want me to send to : " . $nextIp);
-            print("the data i am sending is : " . self::bytesToString($msg));
-            $RouterAsClient = new Socket($nextIp, $this->RouterPort);
-            $this->dout     = new DataOutputStream($RouterAsClient->getOutputStream());
-            $this->dout->writeInt(l);
-            $this->dout->write(msg);
-            $this->dout->flush();
-            $RouterAsClient->close();
         }
         catch (\Exception $ex) {
             self::$routerlog->severe("Attempt to establish connection with other router failed. Exiting progream.");
